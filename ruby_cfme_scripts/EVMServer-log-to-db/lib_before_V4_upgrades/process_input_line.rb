4b5281ae102c5303075f146a134323a8a7af3245
Jared Deubel <jdeubel@redhat.com>
2015-03-30 14:29:58 -0400

    Removing more files

--- ruby_cfme_scripts/EVMServer-log-to-db/lib_before_V4_upgrades/process_input_line.rb
+++
@@ -1,7 +0,0 @@
-=begin rdoc
-Copyright 2008 ManageIQ, Inc
-$Id: process_input_line.rb 16597 2009-10-12 15:36:47Z thennessy $
-=end
-def process_input_line(input_line)
-#  if $gross_logfile_line_cnt = 0 then
-end
