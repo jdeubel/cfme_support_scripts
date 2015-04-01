3f7c7b5fcf42844b4a2cdabbbd4321a8a886a084
Jared Deubel <jdeubel@redhat.com>
2015-04-01 15:53:04 -0400

    Removing file

--- cfme_process_vmstat_output.sh
+++ cfme_process_vmstat_output.sh
@@ -1,2 +1,3 @@
 #Process vmstat output
-ruby  "/home/jdeubel/projects/cfme_support_scripts/ruby_cfme_scripts/process_vmstat_output/vmstat_output.rb" vmstat_output.log 1>vmstat_output_sstdout.lst 2>&1
+#ruby  "/home/jdeubel/projects/cfme_support_scripts/ruby_cfme_scripts/process_vmstat_output/vmstat_output.rb" 
+ruby  "ruby_cfme_scripts/process_vmstat_output/vmstat_output.rb" 
